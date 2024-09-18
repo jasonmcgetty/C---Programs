6182cb4d2d6690a8b82628ef4639c4c6814ec52e
jasontmcgetty <jasontmcgetty@yahoo.com>
2024-09-18 14:57:32 -0400

    Updated Hello World Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello world! My name is Jason" << endl;
+    cout << "Input the number 1 and I will print out one" << endl;
+    int num;
+    cin >> num;
+    if (num==1)
+    {
+        cout << "One" << endl;
+    }
+    return 0;
+}
